a732f1dbd3795c5ea35bc7d930c92725a0b41eb3
Dmitrii_Belov1 <Dmitrii_Belov1@epam.com>
2023-06-27 19:37:34 +0600

    Added fields to open ai settings

--- models/integration_pd.py
+++ models/integration_pd.py
@@ -8,6 +8,10 @@ import openai
 
 class IntegrationModel(BaseModel):
     api_token: SecretField | str
+    model_name: str = 'text-davinci-003'
+    temperature: float = 1.0
+    max_tokens: int = 7
+    top_p: float = 0.8
 
     def check_connection(self):
         openai.api_key = self.api_token.unsecret(session_project.get())
