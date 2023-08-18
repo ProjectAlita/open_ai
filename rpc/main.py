0591b591506a881675a3ad8242b53896bfbae7c7
Usmonbek Ravshanov <usmonbek_ravshanov@epam.com>
2023-08-18 15:53:37 +0500

    predict rpc function added

---
+++ rpc/main.py
@@ -0,0 +1,38 @@
+from pylon.core.tools import log  # pylint: disable=E0611,E0401
+from pylon.core.tools import web
+
+from tools import rpc_tools
+from ..models.integration_pd import IntegrationModel
+from pydantic import ValidationError
+import openai
+from ...integrations.models.pd.integration import SecretField
+
+
+class RPC:
+    integration_name = 'open_ai'
+
+    @web.rpc(f'{integration_name}__predict')
+    @rpc_tools.wrap_exceptions(RuntimeError)
+    def predict(self, project_id, settings, text_prompt):
+        """ Predict function """
+        try:
+            settings = IntegrationModel.parse_obj(settings)
+        except ValidationError as e:
+            return {"ok": False, "error": e}
+
+        try:
+            api_key = SecretField.parse_obj(settings.api_token).unsecret(project_id)
+            openai.api_key = api_key
+            response = openai.Completion.create(
+                model=settings.model_name,
+                prompt=text_prompt,
+                temperature=settings.temperature,
+                max_tokens=settings.max_tokens,
+                top_p=settings.top_p,
+            )
+            result = response['choices'][0]['text']
+        except Exception as e:
+            log.error(str(e))
+            return {"ok": False, "error": "Error in connection"}
+        return {"ok": True, "response": result}
+
