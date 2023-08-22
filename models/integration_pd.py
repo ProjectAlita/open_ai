84913704eeb92528f39f4563b38a7c13cb4de549
Usmonbek Ravshanov <usmonbek_ravshanov@epam.com>
2023-08-22 14:13:39 +0500

    Settings validation rpc function was added

--- models/integration_pd.py
+++ models/integration_pd.py
@@ -6,6 +6,13 @@ from ...integrations.models.pd.integration import SecretField
 import openai
 
 
+class OpenAISettings(BaseModel):
+    model_name: str = 'text-davinci-003'
+    temperature: float = 1.0
+    max_tokens: int = 7
+    top_p: float = 0.8
+
+
 class IntegrationModel(BaseModel):
     api_token: SecretField | str
     model_name: str = 'text-davinci-003'
--- rpc/main.py
+++ rpc/main.py
@@ -2,7 +2,7 @@ from pylon.core.tools import log  # pylint: disable=E0611,E0401
 from pylon.core.tools import web
 
 from tools import rpc_tools
-from ..models.integration_pd import IntegrationModel
+from ..models.integration_pd import IntegrationModel, OpenAISettings
 from pydantic import ValidationError
 import openai
 from ...integrations.models.pd.integration import SecretField
@@ -35,4 +35,12 @@ class RPC:
             log.error(str(e))
             return {"ok": False, "error": "Error in connection"}
         return {"ok": True, "response": result}
-
+    
+    @web.rpc(f'{integration_name}__parse_settings')
+    @rpc_tools.wrap_exceptions(RuntimeError)
+    def parse_settings(self, settings):
+        try:
+            settings = OpenAISettings.parse_obj(settings)
+        except ValidationError as e:
+            return {"ok": False, "error": e}
+        return {"ok": True, "item": settings}
